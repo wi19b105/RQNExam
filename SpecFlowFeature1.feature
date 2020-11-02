1374bb7d6cef496d3ecb534e794fc6a67e983a89
wi19b105 <72461980+wi19b105@users.noreply.github.com>
2020-11-02 19:10:38 +0100

    Add files via upload

---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,21 @@
+﻿Feature: SpecFlowFeature1
+	Entwicklung eines Taschenrechner Projekts für trigonometrische Funktionen
+
+@mytag
+Scenario: sin(pi)
+	Given the number is pi
+	And I choose sinus
+	When the calculator calculates
+	Then the result should be 0
+
+	Scenario: cosinus(pi)
+	Given the number is pi
+	And I choose cosinus
+	When the calculator calculates
+	Then the result should be -1
+
+	Scenario:  tangens(pi)
+	Given the number is pi
+	And I choose tangens
+	When the calculator calculates
+	Then the result should be 0
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+using System.Linq;
+
+namespace ExamRQE
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number;
+        String choose;
+        [Given(@"the number is pi")]
+        public void GivenTheNumberIsPi()
+        {
+            number = Math.PI;
+                
+        }
+        
+        [Given(@"I choose sinus")]
+        public void GivenIChooseSinus()
+        {
+            choose = "sin";
+        }
+        
+        [Given(@"I choose cosinus")]
+        public void GivenIChooseCosinus()
+        {
+            choose = "cos";
+        }
+        
+        [Given(@"I choose tangens")]
+        public void GivenIChooseTangens()
+        {
+            choose = "tan";
+        }
+        
+        [When(@"the calculator calculates")]
+        public void WhenTheCalculatorCalculates()
+        {
+           switch(choose)
+            {
+               case "sin":
+                    Assert.Equal(Math.Sin(number), 0, 5);   
+                break;
+                case "cos":
+                    Assert.Equal(Math.Sin(number), -1, 5);
+                    break;
+                case "tan";
+                    Assert.Equal(Math.Sin(number), 0, 5);
+                    break;
+                default:
+                   break;
+                
+            }
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
+//asd
